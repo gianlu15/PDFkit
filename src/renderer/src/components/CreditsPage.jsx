06b081902435beccc122bee0bb8cec4f37b1ec48
gianluca <tondo.gianlu@gmail.com>
2025-05-27 01:04:27 +0200

    Fixed some bugs like Credits page and updated logo icon

--- src/renderer/src/components/CreditsPage.jsx
+++ src/renderer/src/components/CreditsPage.jsx
@@ -6,6 +6,16 @@ function CreditsPage() {
   return (
     <div className="credits-page-container">
       <div className="credits-information-container">
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy
+          of this software and associated documentation files (the "Software"), to deal
+          in the Software without restriction, including without limitation the rights
+          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+          copies of the Software, and to permit persons to whom the Software is
+          furnished to do so, subject to the following conditions: <br></br>
+
+          The above copyright notice and this permission notice shall be included in all
+          copies or substantial portions of the Software.</p>
+        <p>PDFkit Copyright (c) 2025</p>
         <p>{t('creditsText1')}</p>
         <div className="information-container">
           <p className="credits">Gianluca Tondo</p>
